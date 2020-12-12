e9d7358a81bcdf1c3fc2d63401ba2a6ef27fc265
RAR-Armando <70813056+RAR-Armando@users.noreply.github.com>
2020-12-12 21:33:55 +0700

    ver1

--- main/js/app.js
+++ main/js/app.js
@@ -2,72 +2,29 @@ const btnToggle = document.querySelector(".themes-btn")
 const body = document.querySelector(".body")
 const navPanel = document.querySelector(".nav-panel")
 const imgBtn = document.querySelector(".img-btn")
-const modal = document.querySelector(".modal")
-const imgClose = document.querySelector(".img-close")
-const bodyImg1 = document.querySelector(".img-card-1")
-const bodyImg2 = document.querySelector(".img-card-2")
-const bodyImg4 = document.querySelector(".img-card-4")
-const bodyImg5 = document.querySelector(".img-card-5")
-const bodyImg6 = document.querySelector(".img-card-6")
-const bodyImg7 = document.querySelector(".img-card-7")
-const imgTheme = document.querySelector(".open-theme")
-const themeCont = document.querySelector(".themes-cont")
+const btnOpen = document.querySelector(".open-theme")
+const themesContent = document.querySelector(".themes-cont")
+const btnZen = document.querySelector(".btn-zen")
+const contentZen = document.querySelector(".content")
+
+
+
+btnOpen.addEventListener("click", function(){
+btnOpen.classList.toggle("open-theme-open")
+themesContent.classList.toggle("themes-cont-open")
+});
 
 btnToggle.addEventListener("click", function(){
 
 btnToggle.classList.toggle("themes-btn-dark")
 body.classList.toggle("dark-body")
 navPanel.classList.toggle("nav-panel-dark")
-
-
-});
-
-imgBtn.addEventListener("click", function(){
-modal.classList.toggle("modal-open")
+btnZen.classList.toggle("themes-btn-dark")
 
 });
 
 
-
-imgClose.addEventListener("click", function(){
-modal.classList.remove("modal-open")
-});
-
-
-bodyImg1.addEventListener("click", function(){
-body.classList.toggle("body-card-1")
-
-});
-
-bodyImg2.addEventListener("click", function(){
-    body.classList.toggle("body-card-2")
-    
-});
-
-
-
-bodyImg4.addEventListener("click", function(){
-    body.classList.toggle("body-card-4")
-    
-});
-
-bodyImg5.addEventListener("click", function(){
-    body.classList.toggle("body-card-5")
-    
-});
-
-bodyImg6.addEventListener("click", function(){
-    body.classList.toggle("body-card-6")
-    
-});
-
-bodyImg7.addEventListener("click", function(){
-    body.classList.toggle("body-card-7")
-    
-});
-
-imgTheme.addEventListener("click", function(){
-imgTheme.classList.toggle("open-theme-open")
-themeCont.classList.toggle("themes-cont-open")
-
+btnZen.addEventListener("click", function(){
+contentZen.classList.toggle("content-zen")
+navPanel.classList.toggle("nav-panel-zen")
 });
